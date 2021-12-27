c315117a6b51bcf39a3fa8108a90875c83df3568
Jithendar9676 <jithendarreddy9676@gmail.com>
2021-12-27 19:24:44 +0530

    update

--- full stack/frontend/Company releted/sendingmails/sending.js
+++ full stack/frontend/Company releted/sendingmails/sending.js
@@ -1,15 +1,15 @@
-var sendmails= require('nodemailer')
+var sendmails=require('nodemailer')
 
 let transport= sendmails.createTransport({
     service:'gmail',
     auth:{
-    user:'oichrxukglsbjpwbgy@kvhrw.com',
-    //password:'Gjr@15155'
+    user:'jithendarreddy9676@gmail.com',
+    password:'Gjr@15155Gjr'
     }
 });
 var mailoptions={
-    form:'oichrxukglsbjpwbgy@kvhrw.com',
-    to:'jithendarreddy9676@gmail.com',
+    form:'jithendarreddy9676@gmail.com',
+    to:'jithendarreddy15155@gmail.com',
     subject:"Hii iam a developer"
 }
 transport.sendMail('mailoptions',(err,sent)=>{
